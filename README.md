43aa09433da10432e443519798b514bafa4189f2
simonhuwiler <webmaster@simonhuwiler.ch>
2025-02-28 15:15:03 +0100

    wip

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Sources
 * [NUTS3](https://ec.europa.eu/eurostat/web/gisco/geodata/statistical-units/territorial-units-statistics)
+* [Coastlines](https://www.eea.europa.eu/data-and-maps/data/eea-coastline-for-analysis-2/gis-data/eea-coastline-polyline)
+* [EuroGlobalMap](https://www.mapsforeurope.org/datasets/euro-global-map)
+* [Countryborders](https://ec.europa.eu/eurostat/web/gisco/geodata/administrative-units/countries)
---
+++ src/consts.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+PATH_NUTS_EU = Path('../data/nuts3/NUTS_RG_20M_2024_4326.geojson')
+PATH_NUTS_UK = Path('../data/nuts3/NUTS_Level_3_January_2018_GCB_in_the_United_Kingdom_2022_-764267078201060548.geojson')
+PATH_BOUNDARIES = Path('../data/europeboundaries.geojson')
+PATH_HEXAGON = Path('../export/hexagons.gpkg')
+PATH_MASTERNUTS = Path('../export/masternuts.gpkg')
+PATH_TIMEDATA_NUTS = Path('../export/timedata-nuts.json')
+PATH_TIMEDATA_HEX = Path('../export/timedata-hex.json')
+PATH_DOCS = Path('../docs')
+PATH_BORDERS = Path('../data/CNTR_RG_20M_2024_4326.geojson')
