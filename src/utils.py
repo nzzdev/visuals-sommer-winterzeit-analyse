9914a5b48a947cfd9622192d99c048895032524c
Simon <webmaster@simonhuwiler.ch>
2025-03-04 16:41:57 +0100

    wip

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import folium
+import datetime
 
 def addTitle(map, title):
     text = """
@@ -8,3 +9,30 @@ def addTitle(map, title):
     """.format(title)
 
     map.get_root().html.add_child(folium.Element(text))
+
+# Convert current to only time like the others
+def dt_to_time(dt):
+    dt = datetime.datetime.strptime(dt.split('+')[0], '%Y-%m-%d %H:%M:%S.%f')
+    return dt.time()
+
+def time_to_time(dt):
+    return datetime.datetime.strptime(dt, '%H:%M:%S.%f').time()
+
+# Calculate difference between time_reference and given time
+def calculate_difference_to_timereference(dt, time_reference):
+    diff = datetime.datetime.combine(datetime.date.min, dt) - datetime.datetime.combine(datetime.date.min, time_reference)
+    return round(diff.total_seconds() / 60)
+
+def categorize_time_to_half_hour_numerical(dt):
+    h = dt.hour
+    if dt.minute < 30:
+        return h
+    else:
+        return h + 0.5
+    
+def categorize_time_to_half_hour(dt):
+    h = dt.hour
+    if dt.minute < 30:
+        return f"{h:02d}:00 - {h:02d}:30"
+    else:
+        return f"{h:02d}:30 - {h+1:02d}:00"  
